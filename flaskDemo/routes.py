5e4d98124df912f92e39971364b2b228c10fde57
Israel Yarce <israelhuerta1227@gmail.com>
2022-05-01 17:32:10 -0500

    changes to update, delete, new, assign_home.html

--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -8,12 +8,13 @@ from flaskDemo.models import User, Post, Matches, Team, Opponent, Sport
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
 
-
+#Looking at the database, I think we query through just matchID since it is the only primary key unlike the last lab -
+# - where it had two primary keys (essn, pno), which led to the results2 join
 @app.route("/")
 @app.route("/home")
 def home():
-    #results= Works_On.query.with_entities(Works_On.essn).all()
-    #return render_template('assign_home.html', joined_m_n = results)
+    results= Matches.query.with_entities(Matches.matchID).all()
+    return render_template('assign_home.html', joined_m_n = results)
     results2 = Matches.query.join(Team,Matches.teamName == Team.teamName) \
                .add_columns(Team.teamName) \
                .join(Opponent, Opponent.oppName == Matches.oppName).add_columns(Opponent.oppName)
@@ -64,7 +65,7 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-
+#Picture for profile, see if we can convert to adding picture to Chicago teams on posts
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -99,19 +100,18 @@ def account():
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
 
-
-@app.route("/assign/<pno>/<essn>/new", methods=['GET', 'POST'])
-def new_assign(pno, essn):
+@app.route("/assign/<matchID>/new", methods=['GET', 'POST'])
+def new_match(matchID):
     form = DeptForm()
     if form.validate_on_submit():
-        assign = Works_On(essn=form.ssn.data, pno=form.pnumber.data,hours=0)
+        match = Matches(score=form.score.data, arena=form.arena.data, matchType=form.matchType.data, status=form.status.data, date=form.date.data, teamName=form.teamName.data, oppName=form.oppName.data, sport=form.sport.data)
         db.session.add(assign)
         db.session.commit()
-        flash('You have added a new assignment!', 'success')
+        flash('You have added a new match!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_dept.html', title='New Assignment',
-                           form=form, legend='New Assignment')
-
+    return render_template('create_dept.html', title='New Match',
+                           form=form, legend='New Match')
+'''
 @app.route("/assign/<pno>/<essn>/new", methods=['GET', 'POST'])
 def assign():
     form = AssignForm()
@@ -129,38 +129,37 @@ def assign():
         flash('Assignment Successful', 'success')
         return redirect(url_for('home'))
     return render_template('assign_home.html', title='Assign Employee to Project', form=form)
+'''
 
+@app.route("/matches/<matchID>")
+def match(matchID):
+    assign = Matches.query.get_or_404([matchID])
+    #return render_template('assign.html', title=str(match.teamName)+"_"+ str(match.oppName)+"_"+ str(match.sport), match=match, now=datetime.utcnow())
+    return render_template('assign.html', title=match.matchID, match=match, now=datetime.utcnow())
 
-@app.route("/matches/<teamName>/<oppName>")
-def match(teamName, oppName, sport):
-    assign = Matches.query.get_or_404([teamName, oppName, sport])
-    return render_template('assign.html', title=str(match.teamName)+"_"+ str(match.oppName)+"_"+ str(match.sport), match=match, now=datetime.utcnow())
-
-
-@app.route("/match/<teamName>/<oppName>/<sport>/update", methods=['GET', 'POST'])
+#Update
+@app.route("/match/<matchID>/update", methods=['GET', 'POST'])
 @login_required
-def update_match(teamName, oppName, sport):
+def update_match(matchID):
     #return "update page under construction"
-    match = Matches.query.get_or_404(teamName, oppName, sport)
-    currentMatch = assign.matchID
+    match = Matches.query.get_or_404(matchID)
+    currentMatch = match.matchID
 #matchID or teamName
     form = MatchUpdateForm()
     if form.validate_on_submit():          # notice we are are not passing the dnumber from the form
         if currentMatch !=form.matchID.data:
             match.score=form.score.data
-        match.arena=form.arena.data
-        match.matchType=form.matchType.data
-        match.status=form.status.data 
-        match.date=form.date.data
-        match.teamName=form.teamName.data
-        match.oppName=form.oppName.data
-        match.sport=form.sport.data
+            match.arena=form.arena.data
+            match.matchType=form.matchType.data
+            match.status=form.status.data 
+            match.date=form.date.data
+            match.teamName=form.teamName.data
+            match.oppName=form.oppName.data
+            match.sport=form.sport.data
         db.session.commit()
         flash('Your Match has been updated!', 'success')
         return redirect(url_for('match', matchID=matchID))
     elif request.method == 'GET':              # notice we are not passing the dnumber to the form
-
-        match.matchID=form.matchID.data
         match.score=form.score.data
         match.arena=form.arena.data
         match.matchType=form.matchType.data
@@ -169,17 +168,17 @@ def update_match(teamName, oppName, sport):
         match.teamName=form.teamName.data
         match.oppName=form.oppName.data
         match.sport=form.sport.data
-    return render_template('create_dept.html', title='Update Department',
-                           form=form, legend='Update Department')
+    return render_template('create_dept.html', title='Update Match',
+                           form=form, legend='Update Match')
 
 
 
-
-@app.route("/matches/<teamName>/<oppName>/<sport>delete", methods=['POST'])
+#Delete
+@app.route("/matches/<matchID>/delete", methods=['POST'])
 @login_required
-def delete_match(teamName, oppName, sport):
-    assign = Matches.query.get_or_404([teamName, oppName, sport])
-    db.session.delete(assign)
+def delete_match(matchID):
+    match = Matches.query.get_or_404([matchID])
+    db.session.delete(match)
     db.session.commit()
     flash('The match has been deleted!', 'success')
     return redirect(url_for('home'))
