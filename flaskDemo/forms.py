76b10704604a01eda0283603bc483b353f530001
ckrabb <64988691+ckrabb@users.noreply.github.com>
2022-05-05 13:44:03 -0500

    Cleaned up code

--- flaskDemo/forms.py
+++ flaskDemo/forms.py
@@ -9,13 +9,8 @@ from flaskDemo.models import User, Matches, getMatches, getMatchesFactory, Team,
 from wtforms.fields import DateField
 
 match = Matches.query.with_entities(Matches.teamName, Matches.arena, Matches.sport).distinct()
-#team = Team.query.with_entities(Team.teamName).distinct()
-#opponent = Opponent.query.with_entities(Opponent.oppName).distinct()
-#sport = Sport.query.with_entities(Sport.sportName).distinct()
-#  or could have used ssns = db.session.query(Department.mgr_ssn).distinct()
-# for that way, we would have imported db from flaskDemo, see above
 
-myChoices4 = [(row[0],row[0]) for row in match]  # change
+myChoices4 = [(row[0],row[0]) for row in match] 
 results=list()
 for row in (match):
     rowDict=row._asdict()
@@ -27,25 +22,12 @@ regex1='^((((19|20)(([02468][048])|([13579][26]))-02-29))|((20[0-9][0-9])|(19[0-
 regex2='|(1[0-2]))-((0[1-9])|(1\d)|(2[0-8])))|((((0[13578])|(1[02]))-31)|(((0[1,3-9])|(1[0-2]))-(29|30)))))$'
 regex=regex1 + regex2
 
-"""
-employeeFname = Employee.query.with_entities(Employee.fname).distinct()
-employeeLname = Employee.query.with_entities(Employee.lname).distinct()
-pnumbers = Project.query.with_entities(Project.pname).distinct()
-
-employeeResults = list()
-for row in employeeFname:
-    rowDict2 = row._asdict()
-    employeeResults.append(rowDict2)
-
-#employeeNames = [(row['fname'], row['lname']) for row in employeeResults]
-EmployeeChoices = [(row[0]) for row in employeeLname]
-ProjectChoices = [(row[0]) for row in pnumbers]
-"""
 
 class AssignForm(FlaskForm):
     Employee = SelectField('Employee')
     Project = SelectField('Project')
     submit = SubmitField('Assign')
+
 
     def selectFieldChoices(self):
         results = db.session.query(Employee).order_by(Employee.lname.asc()).all()
@@ -58,6 +40,7 @@ class AssignForm(FlaskForm):
             projects.append((str(result.pnumber), str(result.pname)))
         self.Employee.choices = employees
         self.Project.choices = projects
+
 
 class RemoveForm(FlaskForm):
     Employee = SelectField('Employee')
@@ -78,8 +61,7 @@ class RemoveForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
@@ -110,6 +92,7 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That UserID is taken. Please choose a different one.')
 
+
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
@@ -118,14 +101,10 @@ class PostForm(FlaskForm):
     
 class MatchUpdateForm(FlaskForm):
 
-#    dnumber=IntegerField('Department Number', validators=[DataRequired()])
     matchID = HiddenField("")
 
     matchID=StringField('Match ID:', validators=[DataRequired(),Length(max=15)])
-#  Commented out using a text field, validated with a Regexp.  That also works, but a hassle to enter ssn.
-#    mgr_ssn = StringField("Manager's SSN", validators=[DataRequired(),Regexp('^(?!000|666)[0-8][0-9]{2}(?!00)[0-9]{2}(?!0000)[0-9]{4}$', message="Please enter 9 digits for a social security.")])
 
-#  One of many ways to use SelectField or QuerySelectField.  Lots of issues using those fields!!
     score = StringField("Match Score", validators=[DataRequired()])  # myChoices defined at top
     arena = SelectField("Arena", choices=myChoices2)
     matchType = StringField("Match Type", validators=[DataRequired()])
@@ -133,19 +112,10 @@ class MatchUpdateForm(FlaskForm):
     teamName = SelectField("Team Name", choices=myChoices)
     oppName = StringField("Opponents Name", validators=[DataRequired()])
     sport = SelectField("Sport", choices=myChoices3)
-# the regexp works, and even gives an error message
-#    mgr_start=DateField("Manager's Start Date:  yyyy-mm-dd",validators=[Regexp(regex)])
-#    mgr_start = DateField("Manager's Start Date")
-
-#    mgr_start=DateField("Manager's Start Date", format='%Y-%m-%d')
     date = DateField("Match Date:", format='%Y-%m-%d')  # This is using the html5 date picker (imported)
     submit = SubmitField('Update this match')
 
-
-# got rid of def validate_dnumber
-
-    def validate_dname(self, matchID):    # apparently in the company DB, dname is specified as unique
-         match = Matches.query.filter_by(matchID=matchID.data).first()
+    def validate_dname(self, matchID): 
          if match and (str(match.matchID) != str(self.matchID.data)):
              raise ValidationError('That match ID is already being used. Please choose a different ID.')
 
@@ -155,7 +125,7 @@ class MatchForm(MatchUpdateForm):
     matchID=IntegerField('Match ID', validators=[DataRequired()])
     submit = SubmitField('Add this match')
 
-    def validate_matchID(self, matchID):    #because dnumber is primary key and should be unique
+    def validate_matchID(self, matchID):   
         match = Matches.query.filter_by(matchID=matchID.data).first()
         if match:
             raise ValidationError('That match ID is taken. Please choose a different one.')
